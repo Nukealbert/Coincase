7b67d0e8a8c2660a2e43310ee52fe78c6f6b2252
Kundan Kumar <39196236+Nukealbert@users.noreply.github.com>
2023-05-09 21:57:50 +0530

    Exchanges components is done, Error handling also implemented

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import {server} from '../index'
-import { Container, HStack, VStack,Image } from '@chakra-ui/react';
+import { Container, HStack, VStack,Image, Heading,Text } from '@chakra-ui/react';
 import Loader from './Loader'
+import ErrorComponent from './ErrorComponent'
 
 function Exchanges() {
   const [exchanges, setExchanges]=useState([]);
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
   useEffect(()=> {
     const fetchExchanges = async () => {
-      const {data} = await axios.get(`${server}/exchanges`)
-      console.log(data)
-      setExchanges(data)
-      setLoading(false)
+      try {
+        const {data} = await axios.get(`${server}/exchanges`)
+        setExchanges(data)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+      }
     };
     fetchExchanges();
 
   }, []);
 
-
+  if(error) return <ErrorComponent message={"Error while fetching Exchanges"} />
   return (
     <Container maxW={'container.xl'}>
       {
@@ -45,8 +51,20 @@ function Exchanges() {
 };
 const ExchangeCard=({name,img,rank,url})=>(
   <a href={url} target='blank'>
-      <VStack>
+      <VStack 
+      w={'52'} 
+      shadow={'lg'} 
+      p={'8'} 
+      borderRadius={'lg'} 
+      transition={'all 0.3s'} 
+      m={'4'}
+      css={{"&:hover":{
+        transform:"scale(1.1)"
+      }}}
+      >
         <Image src={img} w={'10'} h={'10'} objectFit={'contain'} alt={name} />
+        <Heading size={'md'} noOfLines={1}>{rank}</Heading>
+        <Text noOfLines={1}>{name}</Text>
       </VStack>
   </a>
 )
